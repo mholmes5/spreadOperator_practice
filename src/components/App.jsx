57b205ee7f4f837b0014bd750812a96ff446d411
Michael Holmes <michaelholmes618@gmail.com>
2021-04-24 14:29:03 -0500

    initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+
+  function handleNewItem(event) {
+    addNewItem(event.target.value);
+  }
+
+  function handleOnClick(event) {
+    const item = newItem;
+    items.push(item);
+    console.log(items);
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleNewItem} type="text" value={newItem} />
+        <button onClick={handleOnClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          <li></li>
         </ul>
       </div>
     </div>
